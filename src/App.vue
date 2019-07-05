8664aba0cdca0307e5465fc678b330862adad6bf
Ehsan <ehsansouri23@gmail.com>
2019-07-05 15:31:50 +0430

    get list of employees from server at the first time

--- src/App.vue
+++ src/App.vue
@@ -22,26 +22,21 @@ export default {
   },
   data() {
     return {
-      employees: [
-        {
-          id: 1,
-          name: "Richard Hendricks",
-          email: "richard@piedpiper.com"
-        },
-        {
-          id: 2,
-          name: "Bertram Gilfoyle",
-          email: "gilfoyle@piedpiper.com"
-        },
-        {
-          id: 3,
-          name: "Dinesh Chugtai",
-          email: "dinesh@piedpiper.com"
-        }
-      ]
+      employees: []
     };
   },
   methods: {
+    async getEmployees() {
+      try {
+        const response = await fetch(
+          "https://jsonplaceholder.typicode.com/users"
+        );
+        const data = await response.json();
+        this.employees = data;
+      } catch (error) {
+        console.log(error);
+      }
+    },
     addEmployee(employee) {
       const lastId =
         this.employees.length > 0
@@ -59,6 +54,9 @@ export default {
         employee.id === id ? updatedEmployee : employee
       );
     }
+  },
+  mounted() {
+    this.getEmployees();
   }
 };
 </script>
