5edaaccd3b90dfacf313617b35b06d2ff83e57f7
Ehsan <ehsansouri23@gmail.com>
2019-07-05 13:13:00 +0430

    add data in vue component

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app" class="small-container">
     <h1>Employees</h1>
-    <employee-table />
+    <employee-table :employees="employees" />
   </div>
 </template>
 
@@ -11,6 +11,27 @@ export default {
   name: 'app',
   components: {
     EmployeeTable
+  },
+  data() {
+    return {
+      employees: [
+        {
+          id: 1,
+          name: 'Richard Hendricks',
+          email: 'richard@piedpiper.com',
+        },
+        {
+          id: 2,
+          name: 'Bertram Gilfoyle',
+          email: 'gilfoyle@piedpiper.com',
+        },
+        {
+          id: 3,
+          name: 'Dinesh Chugtai',
+          email: 'dinesh@piedpiper.com',
+        },
+      ]
+    }
   }
 }
 </script>
